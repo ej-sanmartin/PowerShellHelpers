6159ca8bc64236847f18357bb5ca5fcca04aaa86
Edgar Jr. San Martin <edgar.sanmartinjr@gmail.com>
2021-05-23 00:21:53 -0400

    Cleaning up conditional statements

--- Set_Global_SMTP_Office365.ps1
+++ Set_Global_SMTP_Office365.ps1
@@ -11,9 +11,9 @@
 
 $admin_email = $args[0]
 $sets_SMTP = $args[1]
-$email_validator = /^\S+@\S+\.\S+$/
+$email_validator = '/^\S+@\S+\.\S+$/'
 
-if(-Not admin_email -match $email_validator){
+if(-Not (admin_email -match $email_validator)){
   throw "First argument is not a valid email"
 }
 
@@ -21,7 +21,7 @@ if($sets_SMTP -ne "true" -Or $sets_SMTP -ne "false"){
   throw "Second argument is invalid. Can only accept 'true' or 'false'"
 }
 
-if(-Not Get-Module -ListAvailable -Name ExchangeOnlineManagement){
+if(-Not (Get-Module -ListAvailable -Name ExchangeOnlineManagement)){
   Install-Module -Name ExchangeOnlineManagement -Scope CurrentUser
 }
 
